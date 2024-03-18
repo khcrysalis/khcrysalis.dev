f1a24d33e9bf0af5a43e5b1e0fb3e725c4ef29ce
Samara <97859147+ssalggnikool@users.noreply.github.com>
2024-03-18 04:40:35 -0700

    begging

--- src/components/Tabswitch.js
+++ src/components/Tabswitch.js
@@ -1,8 +1,14 @@
 var currentPage = window.location.pathname;
-var tabLinks = document.querySelectorAll('.header-tab a');
+    var tabLinks = document.querySelectorAll('.header-tab a');
 
-tabLinks.forEach(function(link) {
-    if (link.getAttribute('href') === currentPage) {
-        link.parentNode.classList.add('active-tab');
-    }
-});
+    tabLinks.forEach(function(link) {
+        if (currentPage === '/' || currentPage === '/index.html') {
+            if (link.getAttribute('href') === '/') {
+                link.parentNode.classList.add('active-tab');
+            }
+        } else {
+            if (link.getAttribute('href') === currentPage) {
+                link.parentNode.classList.add('active-tab');
+            }
+        }
+    });
