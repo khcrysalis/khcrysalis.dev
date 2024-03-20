275445ce2a74150f4ad9ed050497022594a4e9c3
Samara <97859147+ssalggnikool@users.noreply.github.com>
2024-03-19 20:56:55 -0700

    astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-            config: {
-                applyBaseStyles: false,
-            },
-        }),
-    ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
