e6db2d6b7ea0e8e7795c5bd9015ec8a44ca527da
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2024-10-20 20:29:58 -0700

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
+import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
-
+	output: "server",
+	adapter: cloudflare(),
+	integrations: [svelte()],
+	experimental: {
+		viewTransitions: true
+	},
+	markdown: {
+        rehypePlugins: []
+    },
 });
--- src/scripts/time.js
+++ src/scripts/time.js
@@ -2,15 +2,15 @@ function updateTime() {
     var currentTime = new Date();
 
     var timeString = currentTime.toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-      hour12: false,
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit'
+		timeZone: 'America/Los_Angeles',
+		hour12: false,
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit'
     });
 
     document.getElementById("pst-time").textContent = "· " + timeString;
-  }
+}
 
-  setInterval(updateTime, 500);
-  updateTime();
+setInterval(updateTime, 500);
+updateTime();
