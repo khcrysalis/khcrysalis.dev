a2ba0da72da2cb78b1bbb5e87d279b91140c2041
darwinx64 <tiramisyuz@proton.me>
2025-05-23 09:35:07 -0500

    some refactoring, update dependencies, and svg icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-import svelte from "@astrojs/svelte";
 
 export default defineConfig({
 	output: "server",
 	adapter: cloudflare(),
-	integrations: [svelte()],
-	experimental: {
-		viewTransitions: true
-	},
+	integrations: [],
 	markdown: {
         rehypePlugins: []
     },
