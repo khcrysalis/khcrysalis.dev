c182f4a34b52c84c8630e9a8760f17eaf61c40ed
Claration <97859147+claration@users.noreply.github.com>
2026-03-14 03:25:55 -0700

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -182,6 +182,7 @@ export const REPO_ARRAY = [
     "https://api.github.com/repos/khcrysalis/Antrag",
     "https://api.github.com/repos/khcrysalis/Feather",
     "https://api.github.com/repos/khcrysalis/EasyPKG",
+    "https://api.github.com/repos/khcrysalis/Shasta",
     "https://api.github.com/repos/palera1n/palera1n",
     "https://api.github.com/repos/palera1n/palen1x",
     "https://api.github.com/repos/palera1n/loader",
